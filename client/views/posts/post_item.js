ddcc758b69c9f3e5c17bfeff793862813800362f
Ryan Bonhardt <rbonhardt@gmail.com>
2014-07-13 12:49:57 -0400

    added post reordering animation

--- client/views/posts/post_item.js
+++ client/views/posts/post_item.js
@@ -1,9 +1,14 @@
+var POST_HEIGHT = 80;
+var Positions = new Meteor.Collection(null);
+
 Template.postItem.helpers({
   ownPost: function() {
-    //...
+    return this.userId == Meteor.userId();
   },
   domain: function() {
-    //...
+    var a = document.createElement('a');
+    a.href = this.url;
+    return a.hostname;
   },
   upvotedClass: function() {
     var userId = Meteor.userId();
@@ -12,8 +17,27 @@ Template.postItem.helpers({
     } else {
       return 'disabled';
     }
+  },
+  attributes: function() {
+    var post = _.extend({}, Positions.findOne({postId: this._id}), this);
+    var newPosition = post._rank * POST_HEIGHT;
+    var attributes = {};
+    
+    if (! _.isUndefined(post.position)) {
+      var delta = post.position - newPosition;      
+      attributes.style = "top: " + delta + "px";
+      if (delta === 0)
+        attributes.class = "post animate"
+    }
+    
+    Meteor.setTimeout(function() {
+      Positions.upsert({postId: post._id}, {$set: {position: newPosition}})
+    });
+    
+    return attributes;
   }
 });
+
 Template.postItem.events({
   'click .upvotable': function(e) {
     e.preventDefault();
