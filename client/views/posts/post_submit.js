aaf0fc50ce27472a572cfde1c2f094fe0002c812
Ryan Bonhardt <rbonhardt@gmail.com>
2014-07-22 01:10:12 -0400

    fixed posts so check for include http on submit

--- client/views/posts/post_submit.js
+++ client/views/posts/post_submit.js
@@ -3,9 +3,15 @@ Template.postSubmit.events({
     e.preventDefault();
 
     var post = {
-      url: $(e.target).find('[name=url]').val(),
       title: $(e.target).find('[name=title]').val(),
       message: $(e.target).find('[name=message]').val()
+    }
+
+    // grabs url, checks to see if it starts with http or https, if does then assigns http to url
+    var url = $(e.target).find('[name=url]').val();
+    if(!!url){
+      var cleanUrl = (url.substring(0, 7) == "http://" || url.substring(0, 8) == "https://") ? url : "http://"+url;
+      post.url = cleanUrl;
     }
 
     Meteor.call('post', post, function(error, id) {
